7d8345bc944bb7343be09cad6a04cd69f76055f7
John Brown <johndbrown91@gmail.com>
2018-08-06 03:40:00 -0400

    Update build uk and gitignore

--- build-module/build.uk.sh
+++ build-module/build.uk.sh
@@ -4,8 +4,8 @@ sh ./build-module/util/clean.sh
 
 # Add modules
   # App routing and module
-git submodule add https://github.com/brown339/app public
-git config -f .gitmodules submodule.public.branch uk
+git submodule add https://github.com/brown339/app public/app
+git config -f .gitmodules submodule.public/app.branch uk
 
   # Header
 git submodule add https://github.com/brown339/header public/header
